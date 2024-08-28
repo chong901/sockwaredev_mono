a488a5d759a7a5e52921c049584e2621ba529b61
chong <chong.chen901@gmail.com>
2024-08-28 11:19:07 +0800

    feat: Add getTimeUntilArrival function to calculate time difference
    This commit adds the getTimeUntilArrival function to the timeUtil.ts file. The function calculates the time difference between the estimated arrival time and the current time. If the time difference is less than or equal to 60 seconds, it returns "Arr". Otherwise, it returns the number of minutes until arrival. This function will be used to display the estimated time until arrival for buses.

---
+++ src/utils/timeUtil.ts
@@ -0,0 +1,15 @@
+export const getTimeUntilArrival = (estimatedArrival: string) => {
+  if (!estimatedArrival) return "";
+  const arrivalDate = new Date(estimatedArrival);
+  const currentDate = new Date();
+
+  const timeDifference = arrivalDate.getTime() - currentDate.getTime();
+
+  if (timeDifference <= 60 * 1000) {
+    return "Arr";
+  }
+
+  const minutes = Math.floor(timeDifference / (1000 * 60));
+
+  return `${minutes}`;
+};
