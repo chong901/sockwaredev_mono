173f65d6eb39b252b2e2d0e99ee5f9f140edba39
chong <chong.chen901@gmail.com>
2024-08-28 21:35:52 +0800

    refactor: Update BusArrivalInfo component layout for better responsiveness
    The BusArrivalInfo component's layout has been updated to improve responsiveness on different screen sizes. The changes include adjusting the positioning, width, and height of various elements within the component. This refactor ensures that the component displays correctly and optimally on both desktop and mobile devices.

--- src/components/pages/map/BusArrivalInfo.tsx
+++ src/components/pages/map/BusArrivalInfo.tsx
@@ -53,13 +53,13 @@ export const BusArrivalInfo = ({ busStop }: BusArrivalInfoProps) => {
   }, [refetch]);
 
   return (
-    <div className="absolute right-8 top-20 z-[1000] min-w-[320px] max-w-[400px] rounded-lg bg-gradient-to-l from-blue-50 via-blue-100 to-blue-200 shadow-md">
+    <div className="absolute bottom-0 left-1/2 z-[1000] flex h-1/3 w-full -translate-x-1/2 flex-col rounded-lg bg-gradient-to-l from-blue-50 via-blue-100 to-blue-200 shadow-md lg:bottom-[unset] lg:left-[unset] lg:right-8 lg:top-20 lg:h-[unset] lg:min-w-[320px] lg:max-w-[400px] lg:translate-x-[unset]">
       <div className="flex flex-wrap gap-2 p-4">
         <div className="text-3xl font-bold">{busStop.code}</div>
         <div className="text-3xl font-bold">{busStop.description}</div>
       </div>
       <hr className="border-t-2 border-gray-200" />
-      <div className="flex flex-col gap-2 p-4">
+      <div className="flex flex-1 flex-col gap-2 overflow-scroll p-4">
         <div className="flex w-full">
           <div className="text-sm">Bus No</div>
           <div className="ml-auto text-sm">Arriving in (mins)</div>
