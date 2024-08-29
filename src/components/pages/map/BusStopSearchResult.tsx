46dda6846f44e48c9141f6c17b61e12d4d7e5e10
chong <chong.chen901@gmail.com>
2024-08-29 10:00:44 +0800

    feat: add loading and empty display for bus stop search result

--- src/components/pages/map/BusStopSearchResult.tsx
+++ src/components/pages/map/BusStopSearchResult.tsx
@@ -1,5 +1,6 @@
+import { LoadingSpinner } from "@/components/atoms/LoadingSpinner";
 import { SearchBusStopsQuery } from "@/graphql-codegen/frontend/graphql";
-import React from "react";
+import React, { ComponentProps } from "react";
 
 type SearchResultsProps = {
   showSearchResult: boolean;
@@ -7,32 +8,47 @@ type SearchResultsProps = {
   onItemClick: (stop: SearchBusStopsQuery["searchBusStops"][0]) => void;
 };
 
+const Container = ({ className, ...rest }: ComponentProps<"div">) => (
+  <div
+    className={`mt-2 flex max-h-[300px] flex-col overflow-y-scroll rounded-md bg-gradient-to-r from-blue-200 via-blue-100 to-blue-50 shadow-md lg:max-h-[600px] ${className ?? ""}`}
+    {...rest}
+  />
+);
+
 const BusStopSearchResult: React.FC<SearchResultsProps> = ({
   showSearchResult,
   busStops,
   onItemClick,
 }) => {
   if (!showSearchResult) return null;
-
+  if (!busStops)
+    return (
+      <Container className="flex items-center justify-center p-4">
+        <LoadingSpinner />
+      </Container>
+    );
+  if (busStops.length === 0)
+    return (
+      <Container className="flex items-center justify-center p-4 text-2xl">
+        No bus stops found
+      </Container>
+    );
   return (
-    busStops &&
-    busStops.length > 0 && (
-      <div className="mt-2 flex max-h-[300px] flex-col overflow-y-scroll rounded-md bg-gradient-to-r from-blue-200 via-blue-100 to-blue-50 shadow-md lg:max-h-[600px]">
-        {busStops.map((stop, index) => (
-          <div
-            key={stop.code}
-            className="flex gap-4 px-4 py-2 text-2xl hover:cursor-pointer hover:font-bold"
-            onClick={() => onItemClick(stop)}
-          >
-            <div>{stop.code}</div>
-            <div>{stop.description}</div>
-            {index < busStops.length - 1 && (
-              <hr className="border-t border-slate-300" />
-            )}
-          </div>
-        ))}
-      </div>
-    )
+    <Container>
+      {busStops.map((stop, index) => (
+        <div
+          key={stop.code}
+          className="flex gap-4 px-4 py-2 text-2xl hover:cursor-pointer hover:font-bold"
+          onClick={() => onItemClick(stop)}
+        >
+          <div>{stop.code}</div>
+          <div>{stop.description}</div>
+          {index < busStops.length - 1 && (
+            <hr className="border-t border-slate-300" />
+          )}
+        </div>
+      ))}
+    </Container>
   );
 };
 
