18a4c70c90909448885ac1e054bc507a8c9f4ebd
chong <chong.chen901@gmail.com>
2024-09-04 22:29:45 +0800

    feat: setup pwa

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
+import nextPWA from "@ducanh2912/next-pwa";
+
+const withPWA = nextPWA({
+  dest: "public",
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+export default withPWA(nextConfig);
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,36 @@ const Map = dynamic(() => import("@/components/pages/map/MapPage"), {
   ssr: false,
 });
 
+const APP_NAME = "SG Bus Arrival Timing";
+const APP_DEFAULT_TITLE =
+  "Real-Time Singapore Bus Arrival Timings | Find Your Next Bus Easily";
+const APP_TITLE_TEMPLATE = "%s - SG Bus Arrival";
+const APP_DESCRIPTION =
+  "Quickly find real-time bus arrival timings in Singapore. Stay updated with the next bus arrival at your stop. Reliable and easy-to-use Singapore bus timing service.";
+
 export const metadata: Metadata = {
+  // PWA configurations
+  applicationName: APP_NAME,
+  manifest: "/manifest.json",
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title:
+      "Real-Time Singapore Bus Arrival Timings | Find Your Next Bus Easily",
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  openGraph: {
+    type: "website",
+    siteName: APP_NAME,
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+
   title: "Real-Time Singapore Bus Arrival Timings | Find Your Next Bus Easily",
   description:
     "Quickly find real-time bus arrival timings in Singapore. Stay updated with the next bus arrival at your stop. Reliable and easy-to-use Singapore bus timing service.",
