e832a1e449f5e960f02fa35277185a3673dcf8f8
chong <chong.chen901@gmail.com>
2024-09-04 22:29:29 +0800

    fix: bus marker z index

--- src/components/pages/map/BusMarker.tsx
+++ src/components/pages/map/BusMarker.tsx
@@ -20,7 +20,7 @@ export const BusMarker = ({
       position={[parseFloat(bus.Latitude) + 0.0002, parseFloat(bus.Longitude)]}
       icon={divIcon({
         html: `
-        <div class="group !bg-slate-500 hover:!bg-slate-600 !rounded-full !w-12 !h-12 !flex !justify-center !items-center !text-xl text-slate-200 hover:!z-[9999] flex-col border-red-white border-2">
+        <div class="group !bg-slate-500 hover:!bg-slate-600 !rounded-full !w-12 !h-12 !flex !justify-center !items-center !text-xl text-slate-200 !z-[9990] hover:!z-[9999] flex-col border-red-white border-2">
           <div class="${color}">
             ${arrivalTime}
           </div>
@@ -30,7 +30,7 @@ export const BusMarker = ({
           <div class="absolute -bottom-1 w-0 h-0 border-l-[10px] border-l-transparent border-r-[10px] border-r-transparent border-t-8 border-t-slate-500 group-hover:border-t-slate-600"/>
         </div>`,
         className:
-          "!bg-slate-500 hover:!bg-slate-600 !rounded-full !w-12 !h-12 !flex !justify-center !items-center !text-xl text-slate-200 hover:!z-[9999] !z-[999]",
+          "!bg-slate-500 hover:!bg-slate-600 !rounded-full !w-12 !h-12 !flex !justify-center !items-center !text-xl text-slate-200 hover:!z-[9999] !z-[9990]",
       })}
     />
   );
