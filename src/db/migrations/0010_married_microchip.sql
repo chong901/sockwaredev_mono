8c49b58b0f1f0c0c2c6bf83a428a1c2bc4a6a9ab
chong <chong.chen901@gmail.com>
2024-09-05 13:56:29 +0800

    feat: Update bus_route table columns for Saturday and Sunday bus timings

---
+++ src/db/migrations/0010_married_microchip.sql
@@ -0,0 +1,4 @@
+ALTER TABLE "bus_route" ALTER COLUMN "sat_first_bus" SET NOT NULL;--> statement-breakpoint
+ALTER TABLE "bus_route" ALTER COLUMN "sat_last_bus" SET NOT NULL;--> statement-breakpoint
+ALTER TABLE "bus_route" ALTER COLUMN "sun_first_bus" SET NOT NULL;--> statement-breakpoint
+ALTER TABLE "bus_route" ALTER COLUMN "sun_last_bus" SET NOT NULL;
