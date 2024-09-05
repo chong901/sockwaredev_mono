b05428764d445f1b63fae1fda388ed2167cb6f15
chong <chong.chen901@gmail.com>
2024-09-05 15:27:30 +0800

    feat: Add @vercel/speed-insights package for website speed analysis

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import { ApolloWrapper } from "@/app/ApolloWrapper";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -21,6 +22,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <ApolloWrapper>{children}</ApolloWrapper>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
