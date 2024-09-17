05234efed514def152a96bad7303d3005adb6c0a
chong <chong.chen901@gmail.com>
2024-09-17 15:45:12 +0800

    tailwind: add tailwind base configuration

---
+++ packages/tailwind-config/base.ts
@@ -0,0 +1,46 @@
+import { PresetsConfig } from "tailwindcss/types/config";
+
+const config: PresetsConfig = {
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      minWidth: {
+        "1/12": "8.333333%",
+        "2/12": "16.666667%",
+        "3/12": "25%",
+        "4/12": "33.333333%",
+        "5/12": "41.666667%",
+        "6/12": "50%",
+        "7/12": "58.333333%",
+        "8/12": "66.666667%",
+        "9/12": "75%",
+        "10/12": "83.333333%",
+        "11/12": "91.666667%",
+        "12/12": "100%",
+      },
+      maxWidth: {
+        "1/12": "8.333333%",
+        "2/12": "16.666667%",
+        "3/12": "25%",
+        "4/12": "33.333333%",
+        "5/12": "41.666667%",
+        "6/12": "50%",
+        "7/12": "58.333333%",
+        "8/12": "66.666667%",
+        "9/12": "75%",
+        "10/12": "83.333333%",
+        "11/12": "91.666667%",
+        "12/12": "100%",
+      },
+      padding: {
+        "safe-bottom": "calc(0.5rem + env(safe-area-inset-bottom))",
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
