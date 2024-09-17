dae7bf627d6accb2ef3aceb80ea2b8a62788902b
chong <chong.chen901@gmail.com>
2024-09-17 16:12:06 +0800

    sg-bus: fix tailwind config to avoid compiling from node_modules

--- apps/sg-bus/tailwind.config.ts
+++ apps/sg-bus/tailwind.config.ts
@@ -5,7 +5,7 @@ const config: Config = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "../../packages/ui/**/*.{js,ts,tsx}",
+    "../../packages/ui/src/**/*.{js,ts,tsx}",
   ],
   presets: [require("@repo/tailwind-config/base")],
 };
