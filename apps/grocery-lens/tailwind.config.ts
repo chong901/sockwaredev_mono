4c3bba4983bf0b7642ebe72d0f1eae69faa1887a
chong <chong.chen901@gmail.com>
2024-09-17 22:37:40 +0800

    gl: configure tailwind and tsconfig

--- apps/grocery-lens/tailwind.config.ts
+++ apps/grocery-lens/tailwind.config.ts
@@ -1,19 +1,12 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  presets: [require("@repo/tailwind-config/base")],
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "../../packages/ui/src/**/*.{js,ts,tsx}",
   ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
 };
 export default config;
