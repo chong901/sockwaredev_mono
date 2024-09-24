96f3b5f0789a310b6e4ab5edd2e1db493efff4c8
chong <chong.chen901@gmail.com>
2024-09-24 15:56:53 +0800

    gl: refactor - add grocery item service

--- apps/grocery-lens/src/app/api/graphql/(resolvers)/grocery-item-resolver.ts
+++ apps/grocery-lens/src/app/api/graphql/(resolvers)/grocery-item-resolver.ts
@@ -1,37 +1,13 @@
-import { UserService } from "@/app/api/graphql/(services)/user-service";
-import { edgedbClient } from "@/edgedb";
-import e from "@/edgedb/edgeql-js";
+import { GroceryItemService } from "@/app/api/graphql/(services)/grocery-item-service";
 import {
   MutationResolvers,
   QueryResolvers,
 } from "@/graphql-codegen/backend/types";
 
-const defaultGroceryItemReturnShape = {
-  id: true,
-  name: true,
-  store: { id: true, name: true },
-  price: true,
-  amount: true,
-  unit: true,
-  notes: true,
-  labels: {
-    id: true,
-    name: true,
-  },
-  pricePerUnit: true,
-} as const;
-
 export const GroceryItemQueryResolver: Pick<QueryResolvers, "getGroceryItems"> =
   {
     getGroceryItems: async (_, __, { userId }) => {
-      const groceryItems = await e
-        .select(e.GroceryItem, (item) => ({
-          ...defaultGroceryItemReturnShape,
-          filter: e.op(item.owner.id, "=", e.uuid(userId)),
-          order_by: { expression: item.created_at, direction: e.DESC },
-        }))
-        .run(edgedbClient);
-      return groceryItems;
+      return GroceryItemService.getGroceryItems(userId);
     },
   };
 
@@ -40,107 +16,14 @@ export const GroceryItemMutationResolver: Pick<
   "addGroceryItem" | "updateGroceryItem" | "deleteGroceryItem"
 > = {
   addGroceryItem: async (_, { input }, { userId }) => {
-    const currentUser = UserService.getUserQuery(userId);
-    const store = e.select(e.Store, () => ({
-      filter_single: { name: input.store, owner: currentUser },
-    }));
-    const labels =
-      input.labels.length === 0
-        ? e.set()
-        : e.select(e.Label, (label) => {
-            const nameInCondition = e.op(
-              label.name,
-              "in",
-              e.set(...input.labels)
-            );
-            const isCurrentUser = e.op(label.owner.id, "=", currentUser.id);
-            return {
-              filter: e.all(e.set(nameInCondition, isCurrentUser)),
-            };
-          });
-    const grocery = await e
-      .select(
-        e.insert(e.GroceryItem, {
-          amount: input.amount,
-          name: input.itemName,
-          owner: currentUser,
-          price: input.price,
-          store: store,
-          unit: input.unit,
-          notes: input.notes,
-          labels: labels,
-        }),
-        () => defaultGroceryItemReturnShape
-      )
-      .run(edgedbClient);
-    return grocery;
+    return GroceryItemService.addGroceryItem(userId, input);
   },
 
   updateGroceryItem: async (_, { id, input }, { userId }) => {
-    const currentUser = UserService.getUserQuery(userId);
-    const store = e.select(e.Store, () => ({
-      filter_single: { name: input.store, owner: currentUser },
-    }));
-    const labels =
-      input.labels.length === 0
-        ? e.set()
-        : e.select(e.Label, (label) => {
-            const nameInCondition = e.op(
-              label.name,
-              "in",
-              e.set(...input.labels)
-            );
-            const isCurrentUser = e.op(label.owner.id, "=", currentUser.id);
-            return {
-              filter: e.all(e.set(nameInCondition, isCurrentUser)),
-            };
-          });
-    const [grocery] = await e
-      .select(
-        e.update(e.GroceryItem, (item) => ({
-          filter: e.all(
-            e.set(
-              e.op(item.id, "=", e.uuid(id)),
-              e.op(item.owner.id, "=", currentUser.id)
-            )
-          ),
-          set: {
-            amount: input.amount,
-            name: input.itemName,
-            price: input.price,
-            store: store,
-            unit: input.unit,
-            notes: input.notes,
-            labels: labels,
-          },
-        })),
-        () => defaultGroceryItemReturnShape
-      )
-      .run(edgedbClient);
-    if (!grocery) {
-      throw new Error("Grocery item not found");
-    }
-    return grocery;
+    return GroceryItemService.updateGroceryItem(id, userId, input);
   },
 
   deleteGroceryItem: async (_, { id }, { userId }) => {
-    const currentUser = UserService.getUserQuery(userId);
-    const [grocery] = await e
-      .select(
-        e.delete(e.GroceryItem, (item) => ({
-          filter: e.all(
-            e.set(
-              e.op(item.id, "=", e.uuid(id)),
-              e.op(item.owner.id, "=", currentUser.id)
-            )
-          ),
-        })),
-        () => defaultGroceryItemReturnShape
-      )
-      .run(edgedbClient);
-    if (!grocery) {
-      throw new Error("Grocery item not found");
-    }
-    return grocery;
+    return GroceryItemService.deleteGroceryItem(id, userId);
   },
 };
---
+++ apps/grocery-lens/src/app/api/graphql/(services)/grocery-item-service.ts
@@ -0,0 +1,144 @@
+import { UserService } from "@/app/api/graphql/(services)/user-service";
+import { edgedbClient } from "@/edgedb";
+import e from "@/edgedb/edgeql-js";
+import { CreateGroceryItemInput } from "@/graphql-codegen/backend/types";
+
+const defaultGroceryItemReturnShape = {
+  id: true,
+  name: true,
+  store: { id: true, name: true },
+  price: true,
+  amount: true,
+  unit: true,
+  notes: true,
+  labels: {
+    id: true,
+    name: true,
+  },
+  pricePerUnit: true,
+} as const;
+
+export class GroceryItemService {
+  static getGroceryItems = async (userId: string) => {
+    const groceryItems = await e
+      .select(e.GroceryItem, (item) => ({
+        ...defaultGroceryItemReturnShape,
+        filter: e.op(item.owner.id, "=", e.uuid(userId)),
+        order_by: { expression: item.created_at, direction: e.DESC },
+      }))
+      .run(edgedbClient);
+    return groceryItems;
+  };
+
+  static addGroceryItem = async (
+    userId: string,
+    data: CreateGroceryItemInput
+  ) => {
+    const currentUser = UserService.getUserQuery(userId);
+    const store = e.select(e.Store, () => ({
+      filter_single: { name: data.store, owner: currentUser },
+    }));
+    const labels =
+      data.labels.length === 0
+        ? e.set()
+        : e.select(e.Label, (label) => {
+            const nameInCondition = e.op(
+              label.name,
+              "in",
+              e.set(...data.labels)
+            );
+            const isCurrentUser = e.op(label.owner.id, "=", currentUser.id);
+            return {
+              filter: e.all(e.set(nameInCondition, isCurrentUser)),
+            };
+          });
+    const grocery = await e
+      .select(
+        e.insert(e.GroceryItem, {
+          amount: data.amount,
+          name: data.itemName,
+          owner: currentUser,
+          price: data.price,
+          store: store,
+          unit: data.unit,
+          notes: data.notes,
+          labels: labels,
+        }),
+        () => defaultGroceryItemReturnShape
+      )
+      .run(edgedbClient);
+    return grocery;
+  };
+
+  static updateGroceryItem = async (
+    itemId: string,
+    userId: string,
+    data: CreateGroceryItemInput
+  ) => {
+    const currentUser = UserService.getUserQuery(userId);
+    const store = e.select(e.Store, () => ({
+      filter_single: { name: data.store, owner: currentUser },
+    }));
+    const labels =
+      data.labels.length === 0
+        ? e.set()
+        : e.select(e.Label, (label) => {
+            const nameInCondition = e.op(
+              label.name,
+              "in",
+              e.set(...data.labels)
+            );
+            const isCurrentUser = e.op(label.owner.id, "=", currentUser.id);
+            return {
+              filter: e.all(e.set(nameInCondition, isCurrentUser)),
+            };
+          });
+    const [grocery] = await e
+      .select(
+        e.update(e.GroceryItem, (item) => ({
+          filter: e.all(
+            e.set(
+              e.op(item.id, "=", e.uuid(itemId)),
+              e.op(item.owner.id, "=", currentUser.id)
+            )
+          ),
+          set: {
+            amount: data.amount,
+            name: data.itemName,
+            price: data.price,
+            store: store,
+            unit: data.unit,
+            notes: data.notes,
+            labels: labels,
+          },
+        })),
+        () => defaultGroceryItemReturnShape
+      )
+      .run(edgedbClient);
+    if (!grocery) {
+      throw new Error("Grocery item not found");
+    }
+    return grocery;
+  };
+
+  static deleteGroceryItem = async (itemId: string, userId: string) => {
+    const currentUser = UserService.getUserQuery(userId);
+    const [grocery] = await e
+      .select(
+        e.delete(e.GroceryItem, (item) => ({
+          filter: e.all(
+            e.set(
+              e.op(item.id, "=", e.uuid(itemId)),
+              e.op(item.owner.id, "=", currentUser.id)
+            )
+          ),
+        })),
+        () => defaultGroceryItemReturnShape
+      )
+      .run(edgedbClient);
+    if (!grocery) {
+      throw new Error("Grocery item not found");
+    }
+    return grocery;
+  };
+}
