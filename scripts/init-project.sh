f1a392ff017130c379bab2ef96414b2271cc718f
chong <aaa59891@gmail.com>
2024-11-26 14:52:45 +0800

    shared: add tailwindcss-animate in init-project script

--- scripts/init-project.sh
+++ scripts/init-project.sh
@@ -12,18 +12,19 @@ fi
 cd "apps/$1" || { echo "Failed to change directory to $1"; exit 1; }
 
 # Define an array of library names
-libraries=(
+workspace_libraries=(
     "@repo/tailwind-config"
     "@repo/typescript-config"
 )
 
 # Iterate over the array and add each library to devDependencies
-for lib in "${libraries[@]}"; do
+for lib in "${workspace_libraries[@]}"; do
     jq --arg lib "$lib" '.devDependencies[$lib] = "workspace:*"' package.json > tmp.$$.json && mv tmp.$$.json package.json
     echo "added $lib to devDependencies"
 done
 
 pnpm add -D prettier-plugin-tailwindcss
+pnpm add tailwindcss-animate
 
 # Copy all files from init-files to the current directory
 cp -r "$init_files_dir/." .
