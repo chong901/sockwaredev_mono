5899010c65e0293e6afebc63f4fb1b8fc4a09728
chong <aaa59891@gmail.com>
2024-12-04 14:41:42 +0800

    tc: fix type error

--- apps/time-converter/src/app/api/timezones/route.ts
+++ apps/time-converter/src/app/api/timezones/route.ts
@@ -1,8 +1,7 @@
 import { TimezoneService } from "@/app/api/(modules)/timezone/timezone.service";
 import { GetTimezonesResponse } from "@/types/api/timezones";
-import { NextApiHandler } from "next";
 
-export const GET: NextApiHandler = async () => {
+export const GET = async () => {
   return Response.json({
     data: await TimezoneService.fetchTimezones(),
   } as GetTimezonesResponse);
