b4787c5ce82630f0978454bc5eba9e9521c142ea
chong <aaa59891@gmail.com>
2024-12-05 11:50:12 +0800

    gl: setup kysely

---
+++ apps/grocery-lens/src/db/db.ts
@@ -0,0 +1,31 @@
+import { DB } from "@/db/types";
+import { Kysely, PostgresDialect } from "kysely";
+import { Pool } from "pg";
+
+const dialect = new PostgresDialect({
+  pool: new Pool({
+    host: process.env.DB_HOST ?? "localhost",
+    port: parseInt(process.env.DB_PORT ?? "5432"),
+    password: process.env.DB_PASSWORD ?? "1234",
+    user: process.env.DB_USER ?? "postgres",
+    database: process.env.DB_NAME ?? "postgres",
+    max: 10,
+  }),
+});
+
+// Database interface is passed to Kysely's constructor, and from now on, Kysely
+// knows your database structure.
+// Dialect is passed to Kysely's constructor, and from now on, Kysely knows how
+// to communicate with your database.
+export const db = new Kysely<DB>({
+  dialect,
+  log(event): void {
+    if (event.level === "error") {
+      console.log("=============================");
+      console.log(event.query.sql);
+      console.log(event.query.parameters);
+      console.log(event.queryDurationMillis);
+      console.log("=============================");
+    }
+  },
+});
