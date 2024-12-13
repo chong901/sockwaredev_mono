23a0e01b6fad72302daa92465bd9c8205382e7bb
chong <aaa59891@gmail.com>
2024-12-13 14:13:22 +0800

    gl: add label resolver with getLabels and addLabel mutations

--- apps/grocery-lens/src/app/api/graphql/(resolvers)/label-resolver.ts
+++ apps/grocery-lens/src/app/api/graphql/(resolvers)/label-resolver.ts
@@ -1,8 +1,26 @@
 import { LabelService } from "@/app/api/graphql/(services)/label-service";
+import { Label } from "@/app/api/graphql/(types)/(objects)/label";
 import {
   MutationResolvers,
   QueryResolvers,
 } from "@/graphql-codegen/backend/types";
+import { Arg, Ctx, Mutation, Query, Resolver } from "type-graphql";
+
+@Resolver()
+export class LabelResolver {
+  @Query(() => [Label])
+  async getLabels(@Ctx() { userId }: { userId: string }) {
+    return LabelService.getLabels(userId);
+  }
+
+  @Mutation(() => Label)
+  async addLabel(
+    @Ctx() { userId }: { userId: string },
+    @Arg("name") name: string,
+  ) {
+    return LabelService.addLabel(userId, name);
+  }
+}
 
 export const LabelQueryResolver: Pick<QueryResolvers, "getLabels"> = {
   getLabels: async (_, __, { userId }) => {
