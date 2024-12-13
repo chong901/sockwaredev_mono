d5448b3dbd9cd40a58025b546f3beb7d8c70df5a
chong <aaa59891@gmail.com>
2024-12-13 15:26:03 +0800

    gl: refactor: remove unused getLabelsQuery method and clean up label service code

--- apps/grocery-lens/src/app/api/graphql/(services)/label-service.ts
+++ apps/grocery-lens/src/app/api/graphql/(services)/label-service.ts
@@ -1,5 +1,4 @@
 import { db } from "@/db/db";
-import e from "@/edgedb/edgeql-js";
 
 export class LabelService {
   static getUserLabels = async (userId: string) => {
@@ -26,17 +25,5 @@ export class LabelService {
       .returningAll()
       .execute();
     return result[0];
-  };
-
-  static getLabelsQuery = (userId: string, names: string[]) => {
-    if (names.length === 0) return e.set();
-
-    return e.select(e.Label, (label) => {
-      const nameInCondition = e.op(label.name, "in", e.set(...names));
-      const isCurrentUser = e.op(label.owner.id, "=", e.uuid(userId));
-      return {
-        filter: e.all(e.set(nameInCondition, isCurrentUser)),
-      };
-    });
   };
 }
